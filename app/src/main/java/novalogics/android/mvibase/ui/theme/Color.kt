c39dc7e0bba85afafc40368c0f0c7ef66836e6b1
NovaLogics <rajitha.shavinda@gmail.com>
2025-02-20 18:12:31 +0530

    feat(theme): add color component

--- app/src/main/java/novalogics/android/mvibase/ui/theme/Color.kt
+++ app/src/main/java/novalogics/android/mvibase/ui/theme/Color.kt
@@ -2,10 +2,68 @@ package novalogics.android.mvibase.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+object Light {
+    val primary = Color(0xFF6A1B9A) // Deep Purple
+    val onPrimary = Color(0xFFFFFFFF) // White text on primary
+    val primaryContainer = Color(0xFFD1C4E9) // Light Lavender container
+    val onPrimaryContainer = Color(0xFF311B92) // Dark Purple text
+    val secondary = Color(0xFF9575CD) // Muted Purple
+    val onSecondary = Color(0xFFFFFFFF) // White text on secondary
+    val secondaryContainer = Color(0xFFB39DDB) // Lavender container
+    val onSecondaryContainer = Color(0xFF4A148C) // Dark purple text
+    val tertiary = Color(0xFF7986CB) // Muted Blue-Purple
+    val onTertiary = Color(0xFFFFFFFF) // White text on tertiary
+    val tertiaryContainer = Color(0xFFC5CAE9) // Light Blue-Purple container
+    val onTertiaryContainer = Color(0xFF283593) // Dark Blue-Purple text
+    val error = Color(0xFFB00020) // Standard Error Red
+    val errorContainer = Color(0xFFFFCDD2) // Light Red container
+    val onError = Color(0xFFFFFFFF) // White text on error
+    val onErrorContainer = Color(0xFF790000) // Dark Red text
+    val background = Color(0xFFF5F5F5) // Light Grey background
+    val onBackground = Color(0xFF212121) // Dark Grey text
+    val surface = Color(0xFFEDE7F6) // Very Light Purple surface
+    val onSurface = Color(0xFF1C1B1F) // Almost Black text
+    val surfaceVariant = Color(0xFFC5C5C5) // Light Grey surface variant
+    val onSurfaceVariant = Color(0xFF49454F) // Medium Grey text
+    val outline = Color(0xFF757575) // Standard Grey outline
+    val inverseOnSurface = Color(0xFFEDE7F6) // Inverted text color
+    val inverseSurface = Color(0xFF332940) // Inverted surface color
+    val inversePrimary = Color(0xFFF48FB1) // Soft Pinkish Purple
+    val shadow = Color(0xFF000000) // Shadow
+    val surfaceTint = Color(0xFF7E57C2) // Tint color
+    val outlineVariant = Color(0xFF9E9E9E) // Lighter Grey outline
+    val scrim = Color(0xFF000000) // Scrim
+}
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+object Dark {
+    val primary = Color(0xFFD1C4E9) // Light Lavender Purple
+    val onPrimary = Color(0xFF311B92) // Dark Purple text
+    val primaryContainer = Color(0xFF4A148C) // Deep Purple container
+    val onPrimaryContainer = Color(0xFFEDE7F6) // Light Purple text
+    val secondary = Color(0xFFB39DDB) // Soft Purple Grey
+    val onSecondary = Color(0xFF311B92) // Dark Purple text
+    val secondaryContainer = Color(0xFF5E35B1) // Purple container
+    val onSecondaryContainer = Color(0xFFEDE7F6) // Light text
+    val tertiary = Color(0xFF7986CB) // Muted Blue-Purple
+    val onTertiary = Color(0xFFFFFFFF) // White text
+    val tertiaryContainer = Color(0xFF3F51B5) // Dark Blue-Purple container
+    val onTertiaryContainer = Color(0xFFC5CAE9) // Light Blue-Purple text
+    val error = Color(0xFFFFB4AB) // Standard error red
+    val errorContainer = Color(0xFF93000A) // Dark Red container
+    val onError = Color(0xFF690005) // Dark Red text
+    val onErrorContainer = Color(0xFFFFDAD6) // Light Red text
+    val background = Color(0xFF121212) // Dark Grey background
+    val onBackground = Color(0xFFE1E3DF) // Light Grey text
+    val surface = Color(0xFF1C1B1F) // Almost Black surface
+    val onSurface = Color(0xFFEDE7F6) // Light text on dark
+    val surfaceVariant = Color(0xFF49454F) // Medium Grey surface
+    val onSurfaceVariant = Color(0xFFC5C5C5) // Light Grey text
+    val outline = Color(0xFFBDBDBD) // Standard Grey outline
+    val inverseOnSurface = Color(0xFF121212) // Inverted text color
+    val inverseSurface = Color(0xFFD1C4E9) // Inverted surface
+    val inversePrimary = Color(0xFF6A1B9A) // Deep Purple
+    val shadow = Color(0xFF000000) // Shadow
+    val surfaceTint = Color(0xFF7E57C2) // Tint color
+    val outlineVariant = Color(0xFF9E9E9E) // Lighter Grey outline
+    val scrim = Color(0xFF000000) // Scrim
+}
