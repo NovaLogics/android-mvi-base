412dfc435540142810ec4278167239c3991f3c67
NovaLogics <rajitha.shavinda@gmail.com>
2025-02-21 01:12:22 +0530

    feat(nav): update navigation app components

--- app/src/main/java/novalogics/android/mvibase/app/MainActivity.kt
+++ app/src/main/java/novalogics/android/mvibase/app/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.AndroidEntryPoint
 import novalogics.android.mvibase.app.navigation.AppNavigation
+import novalogics.android.mvibase.core.presentation.theme.MVIBaseTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AppNavigation()
+            MVIBaseTheme {
+                AppNavigation()
+            }
         }
     }
 }
--- app/src/main/java/novalogics/android/mvibase/app/navigation/AppNavigation.kt
+++ app/src/main/java/novalogics/android/mvibase/app/navigation/AppNavigation.kt
@@ -1,10 +1,26 @@
 package novalogics.android.mvibase.app.navigation
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
-    NavGraph(navController = navController)
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .navigationBarsPadding()
+            .statusBarsPadding()
+    ) { innerPadding ->
+        NavGraph(
+            navController = navController,
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
 }
