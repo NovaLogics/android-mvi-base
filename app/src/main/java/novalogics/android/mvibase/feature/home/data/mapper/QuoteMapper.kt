93c8b30c42e9b3a17bc365c19cacfcb68aecc7a7
NovaLogics <rajitha.shavinda@gmail.com>
2025-02-22 19:11:27 +0530

    feat(home-screen): add room db data components

--- app/src/main/java/novalogics/android/mvibase/feature/home/data/mapper/QuoteMapper.kt
+++ app/src/main/java/novalogics/android/mvibase/feature/home/data/mapper/QuoteMapper.kt
@@ -1,5 +1,6 @@
 package novalogics.android.mvibase.feature.home.data.mapper
 
+import novalogics.android.mvibase.feature.home.data.local.entity.QuoteEntity
 import novalogics.android.mvibase.feature.home.data.model.QuoteDto
 import novalogics.android.mvibase.feature.home.domain.model.Quote
 
@@ -11,6 +12,32 @@ fun QuoteDto.toDomain(): Quote {
         tags = this.tags,
         authorSlug = this.authorSlug,
         length = this.length ?: 0,
+        dateAdded = this.dateAdded,
+        dateModified = this.dateModified
+    )
+}
+
+fun Quote.toEntity(): QuoteEntity {
+    return QuoteEntity(
+        id = this.id,
+        author = this.author,
+        content = this.content,
+        tags = this.tags,
+        authorSlug = this.authorSlug,
+        length = this.length,
+        dateAdded = this.dateAdded,
+        dateModified = this.dateModified
+    )
+}
+
+fun QuoteEntity.toDomain(): Quote {
+    return Quote(
+        id = this.id,
+        author = this.author,
+        content = this.content,
+        tags = this.tags,
+        authorSlug = this.authorSlug,
+        length = this.length,
         dateAdded = this.dateAdded,
         dateModified = this.dateModified
     )
